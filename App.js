bfb2d9888c69eb24b187981f333fdafcd59e97c4
Stelli <stelli@eproc-gov.tech>
2023-10-29 14:25:18 +0700

    add files

--- App.js
+++ App.js
@@ -1,20 +1,139 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import {
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  Dimensions,
+} from "react-native";
+import { BarCodeScanner } from "expo-barcode-scanner";
+import { Camera } from "expo-camera";
+import axios from "axios";
+
+const deviceHeight = Dimensions.get("window").height;
+const deviceWidth = Dimensions.get("window").width;
+console.log("device", deviceHeight, deviceWidth);
 
 export default function App() {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [scanned, setScanned] = useState(true);
+  const baseUrl = "http://192.168.0.100:3000/api/v1";
+
+  useEffect(() => {
+    (async () => {
+      const data = await BarCodeScanner.requestPermissionsAsync();
+      console.log("test", data);
+      setHasPermission(data.status === "granted");
+    })();
+  }, []);
+
+  const handleBarCodeScanned = ({ type, data }) => {
+    setScanned(true);
+    console.log("data", data, "url", `${baseUrl}/barcodes/${data}`);
+    axios({
+      method: "get",
+      url: `${baseUrl}/barcodes/${data}`,
+    })
+      .then((response) => {
+        console.log("res", response.data);
+        if (!!response.data.barcode) {
+          alert(
+            `Bar code with type ${type} and data ${response.data.barcode} has been scanned!`
+          );
+        }
+      })
+      .catch((error) => {
+        alert(`Bar code ${data} can not be found!`);
+      });
+  };
+
+  const renderCamera = () => {
+    return (
+      <View style={styles.cameraContainer}>
+        <BarCodeScanner
+          onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+          style={[styles.camera, styles.absoluteFillObject]}
+        />
+      </View>
+    );
+  };
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+
+  if (hasPermission === false) {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.text}>Camera permission not granted</Text>
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Welcome to the Barcode Scanner App!</Text>
+      <Text style={styles.paragraph}>Scan a barcode to start your job.</Text>
+      {renderCamera()}
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => setScanned(false)}
+        // disabled={scanned}
+      >
+        <Text style={styles.buttonText}>Scan QR to Start your job</Text>
+      </TouchableOpacity>
     </View>
   );
 }
 
+const opacity = "rgba(0, 0, 0, .6)";
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+  paragraph: {
+    fontSize: 16,
+    marginBottom: 20,
+  },
+  cameraContainer: {
+    width: "80%",
+    aspectRatio: 0.55,
+    overflow: "hidden",
+    borderRadius: 10,
+    marginBottom: 20,
+  },
+  camera: {
+    flex: 1,
+  },
+  button: {
+    backgroundColor: "blue",
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  rectangleContainer: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "transparent",
+  },
+  rectangle: {
+    height: 200,
+    width: 300,
+    borderWidth: 2,
+    borderColor: "red",
+    backgroundColor: "transparent",
   },
 });
